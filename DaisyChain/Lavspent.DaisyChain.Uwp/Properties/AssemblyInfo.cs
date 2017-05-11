a9f3618ad000798e50ca79cc067c1eb68c077b71
Petter Labråten <petter.labraaten@labraaten.com>
2017-05-12 00:35:41 +0200

    Prepared Lavspent.DaisyChain.Uwp for nuget. Version 0.2.1.

--- DaisyChain/Lavspent.DaisyChain.Uwp/Properties/AssemblyInfo.cs
+++ DaisyChain/Lavspent.DaisyChain.Uwp/Properties/AssemblyInfo.cs
@@ -23,9 +23,8 @@ using System.Reflection;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.2.0")]
-[assembly: AssemblyFileVersion("0.2.0")]
-[assembly: AssemblyInformationalVersion("1.0.0-alpha")]
+[assembly: AssemblyVersion("0.2.1")]
+[assembly: AssemblyFileVersion("0.2.1")]
 [assembly: NeutralResourcesLanguage("en")]
 
 //[assembly: ComVisible(false)]
